455035c9dd8ecb4b58802ccdefa22042bd5b21fc
Iaron Aires <iaronrp@hotmail.com>
2021-04-11 17:06:56 -0300

    First Commit

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,29 @@
+import {FaGithub} from 'react-icons/fa';
+import {FiX} from 'react-icons/fi';
+import {signIn, useSession, signOut} from 'next-auth/client'
+import styles from './styles.module.scss';
+
+export function SignInButton() {
+  
+  const [session, loading] = useSession();
+
+  return session ? (
+    <button
+    className={styles.signInButton}
+    onClick={() => signOut()}
+    type='button'>
+      <FaGithub color="#04d361"/>
+      {session.user.name}
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button
+    className={styles.signInButton}
+    type='button'
+    onClick={() => signIn()}
+    >
+      <FaGithub color="#eba417"/>
+      Sign in with Github
+    </button>
+  )
+}
