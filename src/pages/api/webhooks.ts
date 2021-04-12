6399cce267f5f33c79929e9b4347935135387450
devMegadan <dev.megadan@gmail.com>
2021-04-12 17:42:29 -0300

    Three commit

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,7 +1,30 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import {Readable} from 'stream';
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  console.log("Evento recebido")
+async function buffer(readable: Readable) {
+  const chunks = [];
 
-  res.status(200).json({ok: true})
+  for await (const chunk of readable) {
+    chunks.push(
+      typeof chunk === 'string' ? Buffer.from(chunk) : chunk
+    )
+  }
+  return Buffer.concat(chunks);
+}
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if(req.method === 'POST') {
+    const buf = await buffer(req);
+    
+    res.status(200).json({ok: true})
+  } else {
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method not allowed')
+  }
 }
